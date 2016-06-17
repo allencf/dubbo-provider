58fcaab86041627fee7c806d51078bd4b92b3272
chenfei <chenfei@ddkl.cn>
2016-06-17 11:17:12 +0800

    添加新项目

---
+++ allen-dubbo-provider-service-impl/src/main/java/com/allen/dubbo/service/impl/DubboTestServiceImpl.java
@@ -0,0 +1,12 @@
+package com.allen.dubbo.service.impl;
+
+import com.allen.dubbo.service.DubboTestService;
+
+public class DubboTestServiceImpl implements DubboTestService {
+
+	public String sayHello() {
+		
+		return "Hello World !!!";
+	}
+
+}
