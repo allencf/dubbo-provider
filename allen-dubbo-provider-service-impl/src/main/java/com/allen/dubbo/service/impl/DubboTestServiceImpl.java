c5f51eba7f6d3314b375fd71fcf323f3ad66d244
chenfei1 <chenfei1@7F-Chenfei1.globale.com>
2016-11-04 14:17:15 +0800

    添加通过dubbo注解方式注册服务

--- allen-dubbo-provider-service-impl/src/main/java/com/allen/dubbo/service/impl/DubboTestServiceImpl.java
+++ allen-dubbo-provider-service-impl/src/main/java/com/allen/dubbo/service/impl/DubboTestServiceImpl.java
@@ -4,7 +4,8 @@ import org.springframework.stereotype.Service;
 
 import com.allen.dubbo.service.DubboTestService;
 
-@Service
+/*@Service*/
+@com.alibaba.dubbo.config.annotation.Service(version = "1.0.0")
 public class DubboTestServiceImpl implements DubboTestService {
 
 	public String sayHello() {
