e7aabd51665d83b30917331aa82e074d795277cd
chenfei1 <chenfei1@7F-Chenfei1.globale.com>
2016-11-04 16:02:03 +0800

    commit

--- allen-dubbo-provider-service-impl/src/main/java/com/allen/dubbo/service/impl/DubboTestServiceImpl.java
+++ allen-dubbo-provider-service-impl/src/main/java/com/allen/dubbo/service/impl/DubboTestServiceImpl.java
@@ -4,8 +4,8 @@ import org.springframework.stereotype.Service;
 
 import com.allen.dubbo.service.DubboTestService;
 
-/*@Service*/
-@com.alibaba.dubbo.config.annotation.Service(version = "1.0.0")
+@Service
+/*@com.alibaba.dubbo.config.annotation.Service(version = "1.0.0")*/
 public class DubboTestServiceImpl implements DubboTestService {
 
 	public String sayHello() {
