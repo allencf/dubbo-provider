0e509ad0744edee004a39ebcc0f673f37ded05c8
chenfei1 <chenfei1@7F-Chenfei1.globale.com>
2016-11-07 14:09:06 +0800

    提交日志文件代码

--- allen-dubbo-provider-service-impl/src/main/java/com/allen/dubbo/service/impl/DubboTestServiceImpl.java
+++ allen-dubbo-provider-service-impl/src/main/java/com/allen/dubbo/service/impl/DubboTestServiceImpl.java
@@ -1,5 +1,7 @@
 package com.allen.dubbo.service.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import com.allen.dubbo.service.DubboTestService;
@@ -8,9 +10,16 @@ import com.allen.dubbo.service.DubboTestService;
 /*@com.alibaba.dubbo.config.annotation.Service(version = "1.0.0")*/
 public class DubboTestServiceImpl implements DubboTestService {
 
+	private final static Logger logger = LoggerFactory.getLogger(DubboTestServiceImpl.class);
+	
 	public String sayHello() {
-		
+		logger.info("dubboTestService  -----------------------------");
 		return "Hello World !!!";
+	}
+	
+	public static void main(String[] args) {
+		logger.info("test-------------");
+		
 	}
 
 }
